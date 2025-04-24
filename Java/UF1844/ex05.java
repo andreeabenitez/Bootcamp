4995f92981d8da8ec7f5c8f310b1883b8d0f0d58
andreea_benitez <andrea_1996_melli@hotmail.com>
2025-04-24 18:49:28 +0200

    feature: J

--- Java/UF1844/ex05.java
+++ Java/UF1844/ex05.java
@@ -1,5 +1,11 @@
 package Java.UF1844;
 
 public class ex05 {
-    
+    public static void main(String[] args) {
+        int n = 0;
+        while (n <= 20) {
+            System.out.println(n);
+            n += 2;
+        }
+    }
 }
---
+++ Java/UF1844/ex06.java
@@ -0,0 +1,11 @@
+package Java.UF1844;
+
+public class ex06 {
+    public static void main(String[] args) {
+        int contador = 0;
+        do {
+            System.out.println(contador);
+            contador += 5;
+        } while (contador <= 50);
+    }
+}
