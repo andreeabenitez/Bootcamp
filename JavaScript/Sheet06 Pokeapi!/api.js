6480bfa82c2f16c655fb6308455687829c23ae6d
Andrea Benitez Brenes <andrea_1996_melli@hotmail.com>
2025-06-25 10:02:10 +0000

    fix: pokeapi JS

--- JavaScript/Sheet06 Pokeapi!/api.js
+++
@@ -1,7 +0,0 @@
-/* export async function buscarPokemon(query) {
-  
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + query);
-    const data = await response.json();
-
-}
-    /*
